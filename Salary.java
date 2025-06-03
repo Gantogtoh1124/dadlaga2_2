4f597e40f89693cb8749d4be3a4e8aca81165f54
Gantogtoh1124 <gantogtohn240@gmail.com>
2025-06-03 14:08:20 +0800

    daalgavar 2 duusav

--- Salary.java
+++ Salary.java
@@ -1,34 +1,39 @@
 import java.io.IOException;
 import java.util.Scanner;
 
+/**
+ * IMPORTANT: 
+ *      O nome da classe deve ser "Main" para que a sua solução execute
+ *      Class name must be "Main" for your solution to execute
+ *      El nombre de la clase debe ser "Main" para que su solución ejecutar
+ */
 public class Salary {
 
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
 
-        double salary = sc.nextDouble();
-        double reajusteRate = 0;
+        double salario = sc.nextDouble();
+        double percentual;
 
-        if (salary >= 0 && salary <= 400.00) {
-            reajusteRate = 0.15;
-        } else if (salary <= 800.00) {
-            reajusteRate = 0.12;
-        } else if (salary <= 1200.00) {
-            reajusteRate = 0.10;
-        } else if (salary <= 2000.00) {
-            reajusteRate = 0.07;
+        if (salario <= 400.00) {
+            percentual = 15;
+        } else if (salario <= 800.00) {
+            percentual = 12;
+        } else if (salario <= 1200.00) {
+            percentual = 10;
+        } else if (salario <= 2000.00) {
+            percentual = 7;
         } else {
-            reajusteRate = 0.04;
+            percentual = 4;
         }
 
-        double reajusteGanho = salary * reajusteRate;
-        double novoSalario = salary + reajusteGanho;
+        double reajuste = salario * (percentual / 100);
+        double novoSalario = salario + reajuste;
 
         System.out.printf("Novo salario: %.2f%n", novoSalario);
-        System.out.printf("Reajuste ganho: %.2f%n", reajusteGanho);
-        System.out.printf("Em percentual: %d %%\n", (int)(reajusteRate * 100));
+        System.out.printf("Reajuste ganho: %.2f%n", reajuste);
+        System.out.printf("Em percentual: %.0f %%\n", percentual);
 
         sc.close();
     }
-
 }
